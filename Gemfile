ce4efcab703846907c7e6db1c2c9706a731d5a00
lisandroseia <lisandroseia11@gmail.com>
2022-04-27 16:59:41 -0300

    Finish users test cases

--- Gemfile
+++ Gemfile
@@ -70,4 +70,5 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'selenium-webdriver'
   gem 'webdrivers'
+  gem 'rails-controller-testing'
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,36 @@
 require_relative '../rails_helper'
 
 RSpec.describe 'users render correct template', type: :request do
-  it 'runs template' do
-    get '/users'
-    expect(response).to    render_template(:users)
+  context 'Get *index' do
+    before :each do
+      get '/users/index'
+    end
+    it 'runs template' do
+      expect(response).to render_template('users/show')
+    end
+
+    it 'gives correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'gives correct body' do
+      expect(response.body).to include('inside user show')
+    end
+  end
+  context 'Get #index' do
+    before :each do
+      get '/users'
+    end
+    it 'runs template' do
+      expect(response).to render_template('users/index')
+    end
+
+    it 'gives correct response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'gives correct body' do
+      expect(response.body).to include('inside user index')
+    end
   end
 end
