f0b4a9cf1d00e31de704d79d3543f1143ec330df
lisandroseia <lisandroseia11@gmail.com>
2022-05-03 11:17:55 -0300

    Improve classes to update counters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  after_save :increase_likes_counter
 
   def increase_likes_counter
     post.increment!(:likes_counter)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+  after_save :increase_posts_counter
 
   def increase_posts_counter
     author.increment!(:posts_counter)
