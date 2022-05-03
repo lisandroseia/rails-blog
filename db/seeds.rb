48740196de209d296b5d9d35c14dd61c5271d2f1
lisandroseia <lisandroseia11@gmail.com>
2022-05-03 11:06:40 -0300

    create seeds class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+first_user = User.create(name: 'Tom', photo: 'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-photo-183042379.jpg', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-photo-183042379.jpg', bio: 'Teacher from Poland.')
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+third_post = Post.create(author: first_user, title: 'Hello', text: 'This is my third post')
+fourth_post = Post.create(author: first_user, title: 'Hello', text: 'This is my fourth post')
+
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'how are you Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'how is going Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'How you doing Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'we were on a break Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'bye Tom!' )
+
+User.find(1).update(name: 'Thomas')
